0701dafed25463c5b33e681c09f55ee43ac2f451
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 08:21:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,4 +67,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber){
+    
+  }
+  else{
+    return `Sorry, we don\'t have a credit card on file for you.`
+  }
 }
